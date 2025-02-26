907a25d74c217e71628640ce1d2de2240deccf3e
Suleman Baloch <sulemanbaloch@Sulemans-MacBook-Pro-2.local>
2025-02-26 15:16:34 -0500

    Fixed GitHub Pages deployment issue

---
+++ frontend/build/precache-manifest.a91e9b9451149e40d699295d3f9dcddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d1b838d8d433fdb5a4762d7122147c",
+    "url": "/Senior-Project/index.html"
+  },
+  {
+    "revision": "1873a83deda56494f1b2",
+    "url": "/Senior-Project/static/css/main.7fe75b23.chunk.css"
+  },
+  {
+    "revision": "d06b9fd20d58d64ebf0a",
+    "url": "/Senior-Project/static/js/2.59c93710.chunk.js"
+  },
+  {
+    "revision": "fd1a73e11f9c31803a0543049623805b",
+    "url": "/Senior-Project/static/js/2.59c93710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1873a83deda56494f1b2",
+    "url": "/Senior-Project/static/js/main.570e7a82.chunk.js"
+  },
+  {
+    "revision": "43d01336bce06ef49dc0",
+    "url": "/Senior-Project/static/js/runtime-main.59cc6996.js"
+  },
+  {
+    "revision": "5b6db504bfa46788313f43b015b20266",
+    "url": "/Senior-Project/static/media/logo.5b6db504.png"
+  }
+]);
